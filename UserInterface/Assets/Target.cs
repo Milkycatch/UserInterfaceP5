eb572c328ca6116e912bb99076bd93b5eeac3fe3
Joel Gonzalez <754537@student.dusd.net>
2024-04-30 13:43:23 -0700

    fhnskjedcfbn

--- UserInterface/Assets/Target.cs
+++ UserInterface/Assets/Target.cs
@@ -10,6 +10,7 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private float spawnRate = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
