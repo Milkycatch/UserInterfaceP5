0dbb379cff27d58479f661db9e91eb5caf7a6d0f
Joel Gonzalez <145067098+Milkycatch@users.noreply.github.com>
2024-05-01 22:26:30 -0700

    todo el tutorial listo pero no jala

---
+++ UserInterface/Assets/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI scoreText;
+    public Button restartButton;
+    public bool isGameActive;
+    public GameObject TitleScreen;
+    public float spawnRate = 1.0f;
+    private int score;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        { 
+         yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score:" + score ;
+    }
+
+    public void GAmeOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    public void Reset()
+    {
+       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void StartGame()
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate = spawnRate/ difficulty;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        TitleScreen.gameObject.SetActive(false);
+    }
+
+}
--- UserInterface/Assets/Target.cs
+++ UserInterface/Assets/Target.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
-    private float spawnRate = 1.0f;
+
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+   
     // Start is called before the first frame update
     void Start()
     {
         targetRb =  GetComponent<Rigidbody>();
+        
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(),ForceMode.Impulse);
         transform.position = RandomspawnPos();
@@ -26,6 +31,24 @@ public class Target : MonoBehaviour
         
     }
 
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        { 
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("bad"))
+        { 
+            gameManager.GAmeOver();
+        }
+    }
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
